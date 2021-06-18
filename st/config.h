f511b4fe83e053e8b123c927c5e0e3e19ce3e994
htsuyoshi <henriquetsuyoshiy@gmail.com>
2021-06-18 00:46:11 +0000

    Change desktop.png

--- st/config.h
+++ st/config.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char *font = "Roboto Mono:size=14";
+ static char *font = "Roboto Mono:size=14";
+// static char *font = "Source Code Pro,Source Code Pro Semibold";
 // static char *font = "Terminus:size=14";
 static int borderpx = 2;
 
