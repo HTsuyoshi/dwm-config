d3a0ecbf942428502c4cec05da7ca535edba24e0
HTsuyoshiy <henriquetsuyoshiy@gmail.com>
2021-10-25 00:31:14 -0300

    refact(dwm.c, appearence.h): add bar gaps

--- dwm/appearence.h
+++ dwm/appearence.h
@@ -2,6 +2,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const unsigned int gappx     = 10;
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;
