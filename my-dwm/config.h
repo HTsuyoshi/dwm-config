82b82db070479e87299c5230998c6cf2f886020c
HTsuyoshi <henriquetsuyoshiy@gmail.com>
2022-10-15 11:40:38 -0300

    add `alacritty` and `tmux` config

--- my-dwm/config.h
+++ my-dwm/config.h
@@ -45,19 +45,19 @@ static const Layout layouts[] = { /* symbol     arrange function */
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *menucmd[] = { "rofi", "-show", "run", NULL};
-static const char *termcmd[]  = { "st", NULL};
-static const char *termcmdtmux[]  = { "st", "tmux", NULL};
+static const char *termcmd[]  = { "alacritty", NULL};
+static const char *termcmdtmux[]  = { "alacritty", "-e", "tmux", "a", NULL};
 static const char *screenshot[]  = { "flameshot", "gui", NULL};
-static const char *badapple[]  = { "st", "-e", "badapple", NULL};
-static const char *badappleconst[]  = { "cool-retro-term", "-e", "badapple", "-r", "1", NULL};
+//static const char *badapple[]  = { "st", "-e", "badapple", NULL};
+//static const char *badappleconst[]  = { "cool-retro-term", "-e", "badapple", "-r", "1", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = menucmd} },
+	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = termcmdtmux } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = termcmdtmux } },
-	{ MODKEY,                       XK_a,      spawn,          {.v = badapple } },
-	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = badappleconst } },
+//	{ MODKEY,                       XK_a,      spawn,          {.v = badapple } },
+//	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = badappleconst } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = screenshot } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
